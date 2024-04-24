6132759e7295f7ef800573ae3ec7b797a8f7723f
zenialexandre <zenialexandre123@gmail.com>
2024-04-24 15:40:35 -0300

    feat: just commiting

--- generate_keys.py
+++ generate_keys.py
@@ -1,10 +1,14 @@
 import numpy as np
 
-def expand_keys(cipher_key_splitted: list[str]):
+def expand_keys(
+    cipher_key_splitted: list[str]
+) -> any:
     state_matrix: list = make_state_matrix(cipher_key_splitted)
-    print(state_matrix)
+    key_schedule: list[list[str]] = get_generated_key_schedule(state_matrix)
 
-def make_state_matrix(cipher_key_splitted) -> list[list[str]]:
+def make_state_matrix(
+    cipher_key_splitted: list[str]
+) -> list[list[str]]:
     state_matrix: list[list[str]] = []
 
     for line in range(0, 16, 4):
@@ -12,5 +16,7 @@ def make_state_matrix(cipher_key_splitted) -> list[list[str]]:
 
     return state_matrix
 
-def generate_round_keys(state_matrix):
+def get_generated_key_schedule(
+    state_matrix: list[list[str]]
+) -> list[list[str]]:
     round_keys = []
