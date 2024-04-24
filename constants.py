7f2f975552afc3efcf274975a0faedb1f76ebed0
zenialexandre <zenialexandre123@gmail.com>
2024-04-24 12:59:52 -0300

    feat: adding constants / adding status bar and valdiation to the GUI

---
+++ constants.py
@@ -0,0 +1,5 @@
+FILE_PATH: str = 'file_path'
+OUTPUT_FILE_NAME: str = 'output_file_name'
+CIPHER_KEY: str = 'cipher_key'
+OK: str = 'Ok'
+CANCEL: str = 'Cancel'
--- program_gui.py
+++ program_gui.py
@@ -1,4 +1,5 @@
 import PySimpleGUI as psg
+from constants import FILE_PATH, OUTPUT_FILE_NAME, CIPHER_KEY, OK, CANCEL
 
 '''
     This GUI returns a tuple of three 'str'.
@@ -9,22 +10,35 @@ def generate_program_gui() -> tuple[str, str, str]:
     psg.theme('DarkTeal2')
     window_layout: list[list] = [
         [psg.T('')],
-        [psg.Text('Choose the file to be uploaded: '), psg.Input(), psg.FileBrowse(key='file_path')],
+        [psg.Text('Choose the file to be uploaded: '), psg.Input(), psg.FileBrowse(key=FILE_PATH)],
         [psg.T('')],
-        [psg.Text('The name of the output file: '), psg.Input(key='output_file_name')],
+        [psg.Text('The name of the output file: '), psg.Input(key=OUTPUT_FILE_NAME)],
         [psg.T('')],
-        [psg.Text('The cipher key to be used: '), psg.Input(key='cipher_key')],
+        [psg.Text('Example of cipher key: 20,1,94,33,199,0,48,9,31,94,112,40,59,30,100,248')],
+        [psg.Text('The cipher key to be used: '), psg.Input(key=CIPHER_KEY)],
         [psg.T('')],
-        [psg.Button('Ok'), psg.Button('Cancel')]
+        [psg.Button(OK), psg.Button(CANCEL)],
+        [psg.T('')],
+        [psg.StatusBar('', size=(20, 1), text_color='red', key='status')]
     ]
     window: psg.Window = psg.Window('Advanced Encryption Standard', window_layout)
+    prompt: any = window['status'].update
+    input_list: list = [
+        key for key, value in window.key_dict.items() if isinstance(value, psg.Input)
+    ]
 
     while (True):
         event, values = window.read()
 
-        if (event == psg.WIN_CLOSED or event == 'Cancel'):
+        if (event == psg.WIN_CLOSED or event == CANCEL):
             break
-        elif (event == 'Ok'):
-            return values['file_path'], values['output_file_name'], values['cipher_key']
+        elif (event == OK):
+            if (all(map(str.strip, [values[key] for key in input_list]))):
+                if (len(values[CIPHER_KEY].split(',')) == 16):
+                    return values[FILE_PATH], values[OUTPUT_FILE_NAME], values[CIPHER_KEY]
+                else:
+                    prompt('The cipher key must have 16 bytes.')
+            else:
+                prompt('Please, fill in the necessary fields.')
 
     window.close()
