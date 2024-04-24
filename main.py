ef74b6c2bd885fef59517ad5cb93d2d91141ea13
zenialexandre <zenialexandre123@gmail.com>
2024-04-24 10:51:48 -0300

    feat: uploading files to /content

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from program_gui import generate_program_gui
+import os
+import shutil
 
-generate_program_gui()
+file_path: str = generate_program_gui()
+
+def upload_inputted_file(
+    file_path: str
+) -> None:
+    if (os.path.exists(file_path)):
+        file_name: str = os.path.basename(file_path)
+        destionation_path: str = os.path.join('content', file_name)
+        shutil.copy(file_path, destionation_path)
+
+upload_inputted_file(file_path)
--- program_gui.py
+++ program_gui.py
@@ -1,16 +1,25 @@
 import PySimpleGUI as psg
 
-def generate_program_gui() -> None:
+'''
+    This GUI returns the path of the uploaded file.
+'''
+
+def generate_program_gui() -> str:
+    psg.theme('DarkTeal2')
     window_layout: list[list] = [
-        [psg.Text('Upload your archive here: '), psg.InputText()],
+        [psg.T('')],
+        [psg.Text('Choose the file to be uploaded: '), psg.Input(), psg.FileBrowse(key='file_path')],
+        [psg.T('')],
         [psg.Button('Ok'), psg.Button('Cancel')]
     ]
     window: psg.Window = psg.Window('Advanced Encryption Standard', window_layout)
 
     while (True):
-        event, _ = window.read()
+        event, values = window.read()
 
         if (event == psg.WIN_CLOSED or event == 'Cancel'):
             break
+        elif (event == 'Ok'):
+            return values['file_path']
 
     window.close()
