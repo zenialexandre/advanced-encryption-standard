6575f7c7f194c963de426c00d8d75ca89b6417c9
zenialexandre <zenialexandre123@gmail.com>
2024-04-25 11:33:11 -0300

    documentation: adding comments

--- generate_keys.py
+++ generate_keys.py
@@ -20,7 +20,15 @@ def get_generated_key_schedule(
     state_matrix: list[list[str]]
 ) -> list[list[str]]:
     # Isso deveria estar dentro de um for range(10) = 10 round_keys, cada uma com 4 words
+    # Deveria ser algo tipo, round_key = get_round_key() -> isso chama a rotacao..etc...
+    # Ai vai adicionando na key_schedule, e retorna no final
+    key_schedule: list[list[str]] = []
+
     round_key: list[list[str]] = get_round_key_with_rotated_words(state_matrix)
+
+    key_schedule.append(round_key)
+
+    return key_schedule
 
 def get_round_key_with_rotated_words(
     state_matrix: list[list[str]]
