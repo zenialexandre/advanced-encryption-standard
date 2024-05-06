e4572c51c26700389924a5edf89b859bb8e02d39
zenialexandre <zenialexandre123@gmail.com>
2024-05-06 17:14:44 -0300

    feat: starting the ciphering flow

--- cipher.py
+++ cipher.py
@@ -1,8 +1,20 @@
+import os
 import numpy as np
 from binascii import hexlify
+from constants import CONTENT
 
 def ciphering_process(
-    output_file_name: str,
+    input_file_path: str,
+    output_file_path: str,
     key_schedule: list[list[str]]
 ) -> None:
-    print(':)')
+    # O processo comeca pegando o valor do arquivo...
+    with open(input_file_path, 'rb') as data:
+        input_file_path_data: bytes = data.read()
+
+    print(input_file_path_data)
+
+    # Depois, a gente salva no arquivo de saida
+    print(output_file_path)
+    #with open(output_file_path, 'wb') as data:
+        #data.write(input_file_path_data)
--- main.py
+++ main.py
@@ -7,31 +7,35 @@ import shutil
 
 def main() -> None:
     (file_path, output_file_name, cipher_key_splitted) = generate_program_gui()
-    upload_inputted_file(file_path)
+    input_file_path: str = upload_inputted_file(file_path)
     start_ciphering_process(
+        input_file_path,
         output_file_name,
         expand_keys(cipher_key_splitted)
     )
 
 def upload_inputted_file(
     file_path: str
-) -> None:
+) -> str:
     if (os.path.exists(file_path)):
         file_name: str = os.path.basename(file_path)
-
-        if (os.path.exists(f'{CONTENT}/{file_name}')):
-            print('This file is already uploaded!')
-            return
-
         destionation_path: str = os.path.join(CONTENT, file_name)
+
+        if (os.path.exists(destionation_path)):
+            print('This file is already uploaded!')
+            return destionation_path
+
         shutil.copy(file_path, destionation_path)
+        return destionation_path
 
 def start_ciphering_process(
+    input_file_path: str,
     output_file_name: str,
     key_schedule: list[list[str]]
 ) -> None:
     ciphering_process(
-        output_file_name,
+        input_file_path,
+        os.path.join(CONTENT, f'{output_file_name}.bin'),
         key_schedule
     )
 
